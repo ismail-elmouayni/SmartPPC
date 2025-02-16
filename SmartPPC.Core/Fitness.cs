2c9099ac5653cb0f79964d7a62132c76b5eff126
= <E030332@arcelormittal.net>
2025-02-16 22:16:08 +0100

    First implementation

---
+++ SmartPPC.Core/Fitness.cs
@@ -0,0 +1,14 @@
+﻿using GeneticSharp;
+
+namespace DDMRP_AI.Core;
+
+public class Fitness : IFitness
+{
+    public double Evaluate(IChromosome chromosome)
+        => Evaluate((Chromosome)chromosome);
+
+    private double Evaluate(Chromosome chromosome)
+    {
+        return 0;
+    }
+}
---
+++ SmartPPC.Core/SampleController.cs
@@ -0,0 +1,55 @@
+﻿
+using GeneticSharp;
+
+namespace DDMRP_AI.Core;
+
+public class SampleController : ISampleController 
+{
+    protected GeneticAlgorithm GA { get; private set; }
+
+    public IFitness CreateFitness()
+    {
+        throw new NotImplementedException();
+    }
+
+    public IChromosome CreateChromosome()
+    {
+        throw new NotImplementedException();
+    }
+
+    public virtual ITermination CreateTermination()
+    {
+        return new FitnessStagnationTermination(100);
+    }
+
+    public virtual ICrossover CreateCrossover()
+    {
+        return new UniformCrossover();
+    }
+
+
+    public virtual IMutation CreateMutation()
+    {
+        return new UniformMutation(true);
+    }
+
+    public virtual ISelection CreateSelection()
+    {
+        return new EliteSelection();
+    }
+
+    public void Initialize()
+    {
+        throw new NotImplementedException();
+    }
+
+    public void ConfigGA(GeneticAlgorithm ga)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Draw(IChromosome bestChromosome)
+    {
+        throw new NotImplementedException();
+    }
+}
